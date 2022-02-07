756161f97893d68047d6da851d8f1e57bbe29292
saschahaarup <sasc0678@stud.kea.dk>
2022-02-07 20:58:16 +0100

    fejl rettelse

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 // const fil = persongalleri j.son
+const header = document.querySelector(".h1_kategori");
 const url = "https://babushka-dd8a.restdb.io/rest/menu";
 const options = {
   headers: {
     "x-apikey": "600ec2fb1346a1524ff12de4",
   },
 };
-
+document.addEventListener("DOMContentLoaded", start);
 let retter;
 let filter = "alle";
 
 // første funktion der kaldes efter DOM er loaded
-const filterknapper = document.querySelectorAll("nav button");
-filterknapper.forEach((knap) => knap.addEventListener("click", filtrerRetter));
-hentData();
-
+function start() {
+  const filterKnapper = document.querySelectorAll("nav button");
+  filterKnapper.forEach((knap) =>
+    knap.addEventListener("click", filtrerRetter)
+  );
+  hentData();
+}
 // eventlistener knyttet til knapperne der vælger hvad for et filter der er aktivit
 function filtrerRetter() {
   filter = this.dataset.kategori;
   console.log("filter", filter);
-  vis();
+  visRetter();
   header.textContent = this.textContent;
 }
 async function hentData() {
   const resspons = await fetch(url, options);
   retter = await resspons.json();
-  vis();
+  visRetter();
 }
-
 // Funktion der viser retter i listeview
-function vis(retter) {
+function visRetter() {
   const section = document.querySelector("section");
   const template = document.querySelector("template").content;
-  const header = document.querySelector("header h1");
   section.textContent = "";
 
   retter.forEach((ret) => {
+    console.log("kategori", ret.kategori);
     if (filter == ret.kategori || filter == "alle") {
-      console.log("kategori", ret.kategori);
-      const klon = template.cloneNode(true).content;
+      const klon = template.cloneNode(true);
       klon.querySelector(".navn").textContent = ret.navn;
       klon.querySelector(".pris").textContent = ret.pris + "kr.";
       klon.querySelector(".kortbeskrivelse").textContent = ret.kortbeskrivelse;
